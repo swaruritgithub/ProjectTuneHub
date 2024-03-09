6c146af8b950bcc01f8b795f793cc3693cf017b0
swaruritgithub <144419317+swaruritgithub@users.noreply.github.com>
2024-03-09 10:32:54 +0530

    Add files via upload

---
+++ ProjectTuneHub-1/src/main/java/com/kodnest/tunehub/ProjectTuneHub1Application.java
@@ -0,0 +1,12 @@
+package com.kodnest.tunehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTuneHub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTuneHub1Application.class, args);
+	}
+}
